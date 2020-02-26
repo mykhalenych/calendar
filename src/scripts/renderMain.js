9cea8036b8f4160f818ab9e708e13501465bf295
MacBook <Mac@MacBook-Pro-MacBook-2.local>
2020-02-26 16:25:15 +0200

    fix some bugs

--- src/scripts/renderMain.js
+++ src/scripts/renderMain.js
@@ -1,25 +1,38 @@
+import { generateNumberRange } from './storage'
+
+
 const getFields = () => {
 
     return generateNumberRange(1, 24)
-        .map(fieldNumber => `
-            <div 
-                class="multicolumns-column-field" 
-                data-field-number="${fieldNumber}">
-            </div>`)
+        .map(weekHour => `
+                <div 
+                    class="multicolumns-column-field" 
+                    data-week-hour="${weekHour}">
+                </div>`)
         .join('')
 }
 
-const columnElem = document.querySelector('.multicolumns');
-const renderColumns = () => {
+const renderColumns = (myEvent) => {
+    const columnElem = document.querySelector('.multicolumns');
+
     const linesString = getFields()
 
     const sectorsString = generateNumberRange(1, 7)
-        .map(columnNumber => `
+        .map(columnWeek => `
+                <div 
+                    class="multicolumns-column" 
+                    data-week-number="${columnWeek}">
+                ${linesString}</div>`)
+        .join('')
+
+    if (myEvent) {
+        generateNumberRange(1, 7).map(columnWeek => `
             <div 
                 class="multicolumns-column" 
-                data-column-number="${columnNumber}">
-            ${linesString}</div>`)
-        .join('')
+                data-week-number="${columnWeek}"><div class="myEvent">${myEvent}</div>
+            </div>`)
+            .join('')
+    }
 
     columnElem.innerHTML = sectorsString;
 }
@@ -28,27 +41,29 @@ const getHoursFields = () => {
 
     return generateNumberRange(1, 12)
         .map(hourFieldNumber => `
-            <div 
-                class="columnHour-field" 
-                data-field-number="${hourFieldNumber}">
-
-                    
-            </div>`)
+                <div 
+                    class="columnHour-field" 
+                    data-field-number="${hourFieldNumber}">
+    
+    
+                </div>`)
         .join('')
 }
 
-const columnHourElem = document.querySelector('.column');
-const renderHourColumns = () => {
+export const renderHourColumns = () => {
+    const columnHourElem = document.querySelector('.column');
+
     const linesString = getHoursFields()
 
     const sectorsString = generateNumberRange(1, 23)
         .map(columnHour => `
-            <div 
-                class="columnHour" 
-                data-column-number="${columnHour}">
-                ${columnHour + ':00'}
-            </div>`)
+                <div 
+                    class="columnHour" 
+                    data-column-number="${columnHour}">
+                    ${columnHour + ':00'}
+                </div>`)
         .join('')
 
     columnHourElem.innerHTML = sectorsString;
 }
+renderHourColumns()
--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -1,8 +1,4 @@
-const columnElem = document.querySelector('.multicolumns');
-const columnHourElem = document.querySelector('.column');
-const createEvent = document.querySelector('#create');
-
-const generateNumberRange = (from, to) => {
+export const generateNumberRange = (from, to) => {
     const result = [];
 
     for (let i = from; i <= to; i++) {
